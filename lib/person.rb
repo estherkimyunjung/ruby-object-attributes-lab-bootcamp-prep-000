9a7c900137e14b9cd89a4631d64f7a093d1a7b1a
YUNJUNG KIM <kyjekffo1105@yahoo.com>
2020-04-14 21:02:05 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,18 +5,15 @@ class Person
   def name=(person_name)
     @name = person_name
   end
-    def name
+  def name
     @name
   end
 
-  # def initialize(job)
-  #   @job = job
-  # end
-  # def job
-  #   @job
-  # end
-  # def job=(person_job)
-  #   @job = person_job
-  # end
-  
+  def job=(person_job)
+    @job = person_job
+  end
+  def job
+    @job
+  end
+
 end
